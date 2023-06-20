61d82e196e9cef3098ef34caa46ee21a7d7467ad
Klim Sukhikh <40332350+sirmong@users.noreply.github.com>
2023-06-20 22:43:58 +0200

    feat: WIP

--- app/src/main/java/com/example/unscramble/ui/GameScreen.kt
+++ app/src/main/java/com/example/unscramble/ui/GameScreen.kt
@@ -82,6 +82,7 @@ fun GameScreen(
             currentScrambledWord = gameUiState.currentScrambledWord,
             userGuess = gameViewModel.userGuess,
             isGuessWrong = gameUiState.isGuessedWordWrong,
+            wordCount = gameUiState.currentWordCount,
             modifier = Modifier
                 .fillMaxWidth()
                 .wrapContentHeight()
@@ -106,7 +107,7 @@ fun GameScreen(
             }
 
             OutlinedButton(
-                onClick = { },
+                onClick = { gameViewModel.skipWord() },
                 modifier = Modifier.fillMaxWidth()
             ) {
                 Text(
@@ -116,7 +117,7 @@ fun GameScreen(
             }
         }
 
-        GameStatus(score = 0, modifier = Modifier.padding(20.dp))
+        GameStatus(score = gameUiState.score, modifier = Modifier.padding(20.dp))
     }
 }
 
@@ -140,6 +141,7 @@ fun GameLayout(
     currentScrambledWord: String,
     userGuess: String,
     isGuessWrong: Boolean,
+    wordCount: Int,
     modifier: Modifier = Modifier
 ) {
     val mediumPadding = dimensionResource(R.dimen.padding_medium)
@@ -159,7 +161,7 @@ fun GameLayout(
                     .background(colorScheme.surfaceTint)
                     .padding(horizontal = 10.dp, vertical = 4.dp)
                     .align(alignment = Alignment.End),
-                text = stringResource(R.string.word_count, 0),
+                text = stringResource(R.string.word_count, wordCount),
                 style = typography.titleMedium,
                 color = colorScheme.onPrimary
             )
--- app/src/main/java/com/example/unscramble/ui/GameUIState.kt
+++ app/src/main/java/com/example/unscramble/ui/GameUIState.kt
@@ -2,5 +2,7 @@ package com.example.unscramble.ui
 
 data class GameUIState(
     val currentScrambledWord: String = "",
-    val isGuessedWordWrong: Boolean = false
+    val isGuessedWordWrong: Boolean = false,
+    val score: Int = 0,
+    val currentWordCount: Int = 1
 )
