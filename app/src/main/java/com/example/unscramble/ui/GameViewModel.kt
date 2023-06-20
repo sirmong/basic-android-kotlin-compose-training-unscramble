03887edecb6a3ca3bb316b5bb510b7d59c921655
Klim Sukhikh <40332350+sirmong@users.noreply.github.com>
2023-06-20 20:12:42 +0200

    view model

--- app/src/main/java/com/example/unscramble/ui/GameViewModel.kt
+++ app/src/main/java/com/example/unscramble/ui/GameViewModel.kt
@@ -14,13 +14,31 @@ class GameViewModel : ViewModel() {
 
     private val usedWords = mutableSetOf<String>()
 
+    init {
+        resetGame()
+    }
+
     private fun pickRandomWordAndShuffle(): String {
         currentWord = allWords.random()
         return if (usedWords.contains(currentWord)) {
             pickRandomWordAndShuffle()
         } else {
             usedWords.add(currentWord)
-            shuffleCurrentWord(currentWord)
+            return shuffleCurrentWord(currentWord)
         }
+    }
+
+    private fun shuffleCurrentWord(word: String): String {
+        val tempWord = word.toCharArray()
+        tempWord.shuffle()
+        while (String(tempWord).equals(word)) {
+            tempWord.shuffle()
+        }
+        return String(tempWord)
+    }
+
+    fun resetGame() {
+        usedWords.clear()
+        _uiState.value = GameUIState(currentScrambledWord = pickRandomWordAndShuffle())
     }
 }
